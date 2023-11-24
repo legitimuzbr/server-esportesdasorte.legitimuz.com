1cec765418dcc50ad8ce961995768f1bb5d92ce0
advmaster <118003523+advmaster@users.noreply.github.com>
2023-11-24 18:15:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://sdk-api.legitimuz.com.br');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
