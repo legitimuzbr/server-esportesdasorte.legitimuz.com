3973ba1bc101ff80e31ecfa218bc3816eed9d6df
advmaster <118003523+advmaster@users.noreply.github.com>
2023-11-24 17:12:51 -0300

    a

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://mateus:mZ4RBNdxAkLzO9I4@legitimuz.hpaophu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexão com MongoDB estabelecida com sucesso'))
+  .catch(err => console.error('Não foi possível conectar ao MongoDB:', err));
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const userSchema = new mongoose.Schema({
+  cpf: String,
+  lastAction: Date
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/user', async (req, res) => {
+  const { cpf } = req.body; // Assume que o CPF é enviado no corpo da requisição
+
+  try {
+    // Verifique se o usuário já existe
+    const user = await User.findOne({ cpf });
+
+    if (user) {
+      
+      const now = new Date();
+      const lastAction = user.lastAction ? new Date(user.lastAction) : new Date(0);
+      const minutesSinceLastAction = (now - lastAction) / (1000 * 60);
+
+      if (minutesSinceLastAction < 1) {
+        const remainingTimeMs = (1 * 60 * 1000) - (now - lastAction);
+        const remainingMinutes = Math.floor(remainingTimeMs / (1000 * 60));
+        const remainingSeconds = Math.floor((remainingTimeMs % (1000 * 60)) / 1000);
+
+        const timeLeftFormatted = `${remainingMinutes}m ${remainingSeconds}s`;
+        
+        return res.status(200).json({ canVerify: false, message: 'Ação bloqueada', timeLeft: timeLeftFormatted });
+      } else {
+        user.lastAction = now;
+        await user.save();
+        buttonElement = `<button class="btn btn-light w-100 btn-lg" id="legitimuz-action-verify">Iniciar verificação</button>`
+        return res.status(200).json({ canVerify: true, buttonElement: buttonElement, user });
+      }
+    
+    }
+
+    const newUser = new User({ cpf, lastAction: new Date() });
+    await newUser.save();
+
+    res.status(201).json({ canVerify: true, user: newUser });
+
+  } catch (error) {
+
+    res.status(500).send(error.message);
+
+  }
+});
+
+
+
+
+app.use((req, res) => {
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,71 @@
+const legitimuz = Legitimuz({
+  host: "https://api.legitimuz.com",
+  token: "7a893ced-4ab7-4dec-8e9e-9bf33db1cc80"
+});
+
+legitimuz.mount();
+
+const nameInput = document.getElementById('legitimuz-hydrate-name');
+
+  function checkForValue() {
+
+    if (nameInput.value) {
+
+      const cpf = document.getElementById("legitimuz-hydrate-cpf").value
+
+      fetch('/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cpf }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.error('Erro na resposta da requisição');
+          }
+        })
+        .then((data) => {
+          if (!data.canVerify) {
+            console.log('Não autorizado');
+            document.querySelector("#aviso").classList.remove("d-none");
+            document.querySelector("#contador").innerText = `Tente novamente em: ${data.timeLeft}`;
+          } else {
+            console.log('Autorizado');
+            
+            document.getElementById("botao").innerHTML = data.buttonElement;
+            legitimuz.mount()
+
+            document.getElementById("legitimuz-hydrate-cpf").disabled = true;
+          }
+        })
+        .catch((error) => {
+          console.error('Erro na requisição', error);
+        });
+
+    } else {
+      
+      setTimeout(checkForValue, 1000);
+
+    }
+  }
+
+  checkForValue();
+
+
+function legitimuzLinkOn() {
+  $("#brand").css({
+    textDecoration: 'underline',
+    cursor: 'pointer'
+  });
+}
+
+function legitimuzLinkOut() {
+  $("#brand").css('textDecoration', 'none');
+}
+
+function legitimuzLink() {
+  window.open("https://legitimuz.com/", "_blank");
+}
