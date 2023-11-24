421c1517813a4aa81b600091d63c51835fcfd0d2
advmaster <118003523+advmaster@users.noreply.github.com>
2023-11-24 17:14:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://esportesdasorte.legitimuz.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
